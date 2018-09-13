5bb66e464308c78ab0820ae30b1ecc1aa6d99625
Shrirang Patil <shrirangpatil1996@gmail.com>
2018-09-13 23:04:40 +0530

    changes in content

--- resume/app/forms.py
+++ resume/app/forms.py
@@ -1,8 +1,6 @@
 #-*- coding: utf-8 -*-
 from django import forms
 from app import static
-class homeForm(forms.Form):
-	myImage = forms.ImageField(label= "app/me.jpg")
 
 class educationForm(forms.Form):
 	workExperience = [["Company","Designation","Duration"],["Informant Networks","Intern","Jun-2018 to Aug-2018"]]
@@ -13,7 +11,8 @@ class educationForm(forms.Form):
 	achievements = [["Secured 26th position in Rivigo Coding Championship 2018"],["Secured 3rd position in Avalanche 2017"]]
 	certificate = [["Business English Certificate Preliminary in B1 level"],
 	["Programming, Data Structure, Algorithms Using Python NPTEL-IIT Madras"]]
-	skills = [["Language","Software","Database"],["Python, Java, C","Git, Vim, NetBeans,Pycharm","Postgresql, Sqlite3"]]
+	skills = [["Language","Software","Database","FrameWorks"],
+	["Python, Java, C","Git, Vim, NetBeans,Pycharm,Android Studio","Postgresql, Sqlite3","Django-2.1"]]
 
 class contactForm(forms.Form):
 	contact = [["Mail","shrirangpatil1996@gmail.com"],["Github","github.com/ShrirangPatil"]]
